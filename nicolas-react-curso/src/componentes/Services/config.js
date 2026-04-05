154f7dbd0eb26633a6f4b2729434b383af9494b7
Nicolas Racca <132503784+nikittoo@users.noreply.github.com>
2026-04-05 20:42:32 -0300

    Borre los datos de la FireBase

--- nicolas-react-curso/src/componentes/Services/config.js
+++ nicolas-react-curso/src/componentes/Services/config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBIouuiW-XBwpx6-I7odZaya3AIxG0yGlY",
-  authDomain: "nicolas-react1.firebaseapp.com",
-  projectId: "nicolas-react1",
-  storageBucket: "nicolas-react1.firebasestorage.app",
-  messagingSenderId: "608373828963",
-  appId: "1:608373828963:web:4b7d1a93b1c6e28b08f756"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
